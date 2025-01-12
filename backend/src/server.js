3c49fce48212cf7ee9e9c1f327764b9a7cad0c50
Vedant Kesarwani <relativityeismc2@gmail.com>
2025-01-12 21:15:17 +0530

    added utils

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,27 +1,15 @@
 import dotenv from 'dotenv'
-import express from 'express'
 import connectDB from './db/index.js'
+import app from './app.js'
 
 dotenv.config({
     path: './env'
 })
 
-connectDB()
-
-// const app = express()
-
-// ;(async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         app.on('error', (error) => {
-//             console.log('ERR: ', error)
-//             throw error
-//         })
-//         app.listen(process.env.PORT, () => {
-//             console.log(`App listening on port ${process.env.PORT}`)
-//         })
-//     } catch (err) {
-//         console.error('ERROR: ', err)
-//         throw err
-//     }
-// })()
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+}).catch((err) => {
+    console.log('MongoDB connection failed!!', err)
+})
