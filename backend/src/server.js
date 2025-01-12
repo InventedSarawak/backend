56a2dc1e17c9cd99f6bb4e3f43302bbcef788354
Vedant Kesarwani <relativityeismc2@gmail.com>
2025-01-12 19:52:22 +0530

    mongo connection

---
+++ backend/src/server.js
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import connectDB from './db/index.js'
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+// const app = express()
+
+// ;(async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on('error', (error) => {
+//             console.log('ERR: ', error)
+//             throw error
+//         })
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App listening on port ${process.env.PORT}`)
+//         })
+//     } catch (err) {
+//         console.error('ERROR: ', err)
+//         throw err
+//     }
+// })()
